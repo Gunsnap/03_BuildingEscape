b62d134a35d3e64a118a3def221808613ce1a5b6
Daniel V. Sandholt <gunsnap@gmail.com>
2017-01-27 14:25:19 +0100

    Start of walls.

--- BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -1,19 +1,15 @@
-
-
-#pragma once
-
 #include "Components/ActorComponent.h"
-#include "PositionReport.generated.h"
+#include "OpenDoor.generated.h"
 
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
-class BUILDINGESCAPE_API UPositionReport : public UActorComponent
+class BUILDINGESCAPE_API UOpenDoor : public UActorComponent
 {
 	GENERATED_BODY()
 
 public:	
 	// Sets default values for this component's properties
-	UPositionReport();
+	UOpenDoor();
 
 	// Called when the game starts
 	virtual void BeginPlay() override;
